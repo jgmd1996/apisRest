9cc2396b518ac16a067b0a0f95330c5a95e07a41
Your Name <you@example.com>
2022-10-02 20:11:51 -0300

    Adicinado o metado post no arquivo de controole

--- atividades/nonaAtividade.js
+++ atividades/nonaAtividade.js
@@ -1,3 +1,5 @@
+// controller de livro 
+
 import livros from "../models/Livro.js";
 
 class LivroController {
@@ -22,4 +24,25 @@ class LivroController {
     }
 }
 
-export default LivroController;
+//export default LivroController;
+
+
+/// rotas de livro
+
+import express from "express";
+import LivroController from "../controllers/livrosController.js";
+
+const router = express.Router();
+
+router
+.get("/livros", LivroController.listarLivros)
+.post("/livros", LivroController.cadastrarLivro)
+
+//export default router;
+
+
+
+//exclusao do metado post no index.js
+
+
+
